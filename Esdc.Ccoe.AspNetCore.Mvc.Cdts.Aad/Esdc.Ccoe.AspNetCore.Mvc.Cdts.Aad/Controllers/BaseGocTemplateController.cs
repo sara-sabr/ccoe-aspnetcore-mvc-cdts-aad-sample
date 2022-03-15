045f07e2359c5998e575022d80647c6175a2cfb9
Nicolas Rodrigues <nicolas.rodrigues@hrsdc-rhdcc.gc.ca>
2022-03-15 10:53:54 -0400

    + Final initial commit

---
+++ Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Controllers/BaseGocTemplateController.cs
@@ -0,0 +1,39 @@
+﻿using GoC.WebTemplate.Components.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad.Controllers
+{
+    public abstract class BaseGocTemplateController : Controller
+    {
+        private readonly IModelAccessor _modelAccessor;
+
+        public BaseGocTemplateController(IModelAccessor modelAccessor)
+        {
+            _modelAccessor = modelAccessor;
+        }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext filterContext, ActionExecutionDelegate next)
+        {
+            ViewBag.WebTemplateModel = _modelAccessor.Model;
+            _modelAccessor.Model.HeaderTitle = "CCoE Sample App";
+            _modelAccessor.Model.ApplicationTitle.Text = "CCoE | CDTS-AAD-MVC Sample";
+            _modelAccessor.Model.ApplicationTitle.Href = Url.Action("Index", "Home");
+            
+            // To add account settings button.
+            _modelAccessor.Model.AppSettingsURL = Url.Action("Index", "Home");
+
+            await base.OnActionExecutionAsync(filterContext, next);
+
+            if (User.Identity.IsAuthenticated) {
+                _modelAccessor.Model.ShowSignOutLink = true;
+                _modelAccessor.Model.Settings.SignOutLinkUrl = Url.Action(nameof(AccountController.SignOut), "Account");
+
+            } else {
+                _modelAccessor.Model.ShowSignInLink = true;
+                _modelAccessor.Model.Settings.SignInLinkUrl = Url.Action(nameof(AccountController.SignIn), "Account");
+            }
+        }
+    }
+}
--- Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Startup.cs
+++ Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad/Startup.cs
@@ -1,13 +1,14 @@
+using GoC.WebTemplate.Components.Core.Services;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using GoC.WebTemplate.Components.Core.Services;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using System.Threading.Tasks;
 
 namespace Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad
 {
@@ -20,7 +21,6 @@ namespace Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // For GoC.WebTemplate.
@@ -29,8 +29,33 @@ namespace Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad
             // For GoC.WebTemplate.
             services.ConfigureGoCTemplateRequestLocalization();
 
-            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+            var initialScopes = Configuration["DownstreamApi:Scopes"]?.Split(' ');
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
+                // You could put "initalScopes" in the parameter of the next method. 
+                // This will ask EVERY permission that your AAD App needs. At first login, it will ask the user to consent for all permissions 
+                // even if that specific user don't need all of them.
+                // If not, the consent of these permissions will be ask incrementally when calling method that needs them.
+                // This is a better way to do it but it depends on your use case and your app, it is up to you.
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddMicrosoftGraph(Configuration.GetSection("DownstreamApi"))
+                // This saves the token of the user in memory. For example, the first time the user logs in after the server is rebooted,
+                // if the user reach the private page, MSAL will request a token (with the scopes specified in the method) to get the Users
+                // and saves it in memory. Next time the user reach the page, MSAL will make the request directly to Graph API except if the token
+                // is expired.
+                .AddInMemoryTokenCaches();
+
+            services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options => {
+                options.Events.OnSignedOutCallbackRedirect = context => {
+                    // This is to redirect to home when the user sign out. It is the simplest way to
+                    // redirect the user when MSAL sign out the user.
+                    context.HttpContext.Response.Redirect(context.Options.SignedOutRedirectUri);
+                    context.HandleResponse();
+
+                    return Task.CompletedTask;
+                };
+
+            });
 
             services.AddControllersWithViews(options => {
                 var policy = new AuthorizationPolicyBuilder()
@@ -38,17 +63,16 @@ namespace Esdc.Ccoe.AspNetCore.Mvc.Cdts.Aad
                     .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
             });
+
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
